41c7fe31eff74677deaec05b34d1512b82090f52
Bruno Domingues <bruno.domingues@lighthouseit.com.br>
2022-11-14 08:36:19 -0300

    feat: prettier and eslint installed and configured

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("listening on port 3000");
+app.listen(443, () => {
+  console.log('Server is listening on port 433');
 });
